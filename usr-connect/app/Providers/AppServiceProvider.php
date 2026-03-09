fff6110696e103b4274052d3568b270381b4187d
Nessprog <ineslaniez31@gmail.com>
2026-03-09 15:55:18 +0100

    FIX: Envoi du build et suppression gitignore

--- usr-connect/app/Providers/AppServiceProvider.php
+++ usr-connect/app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        // Force l'URL des assets pour Railway
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
